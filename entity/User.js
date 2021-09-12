2b6c2991fc804ad568d33a3472f079ee70928f80
ExcelDurant <gnopaexcel@yahoo.fr>
2021-09-12 08:26:32 +0100

    added a User entity

---
+++ entity/User.js
@@ -0,0 +1,28 @@
+var EntitySchema = require("typeorm").EntitySchema;
+
+module.exports = new EntitySchema({
+    name: "User", // Will use table name `category` as default behaviour.
+    tableName: "users", // Optional: Provide `tableName` property to override the default behaviour for table name. 
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true
+        },
+        name: {
+            type: "varchar"
+        },
+        phone_number: {
+            type:"int"
+        },
+        role:{
+            type:"varchar"
+        },
+        registeredOn:{
+            type:"date"
+        },
+        cash:{
+            type:"int"
+        }
+    }
+});
